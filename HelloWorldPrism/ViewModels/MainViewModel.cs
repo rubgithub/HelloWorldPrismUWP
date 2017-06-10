6d16e65675cf515b18dba55e44ad5f054da53e60
rubgithub <rubensatual@hotmail.com>
2017-06-10 18:24:43 -0400

    Navigation

--- HelloWorldPrism/ViewModels/MainViewModel.cs
+++ HelloWorldPrism/ViewModels/MainViewModel.cs
@@ -7,16 +7,40 @@ namespace HelloWorldPrism.ViewModels
     public class MainViewModel : ViewModelBase
     {
         private readonly INavigationService _navigationService;
-        public DelegateCommand NavegarPriximaCommand { get; private set; }
+        public DelegateCommand NavegarProximaCommand { get; }
+        public DelegateCommand NavegarVoltaCommand { get; }
+
+        private bool _isNextEnable = true;
+        public bool IsNextEnable
+        {
+            get { return _isNextEnable; }
+            set { SetProperty(ref _isNextEnable, value); }
+        }
+        private bool _canGoBack;
+        public bool CanGoBack
+        {
+            get { return _canGoBack; }
+            set { SetProperty(ref _canGoBack, value); }
+        }
+
 
         public MainViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
-            NavegarPriximaCommand = new DelegateCommand(NavegarPrixima);
+            NavegarProximaCommand = new DelegateCommand(NavegarProxima).ObservesCanExecute(() => IsNextEnable);
+            NavegarVoltaCommand =   new DelegateCommand(NavegarVolta).ObservesCanExecute(() => CanGoBack);
         }
 
-        private void NavegarPrixima()
+
+        private void NavegarVolta()
         {
+            //_navigationService.GoBack();
+        }
+
+        private void NavegarProxima()
+        {
+            IsNextEnable = false;
+            CanGoBack = true;
             _navigationService.Navigate("First", null);
         }
     }
